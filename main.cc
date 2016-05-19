37a8f82ede9b340fd29e9094160a0bfc4bee4390
MarbhDamhsa <jmendelsohn11@bristolcc.edu>
2016-05-19 12:51:19 -0400

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Asteroid.h"
+
+using namespace std;
+
+
+int main()
+{
+	Asteroid *testAsteroid = new Asteroid(5, 20, -5);
+	cout << endl;
+	testAsteroid -> setSize(2);
+	testAsteroid -> setSpeed(7,-19);
+	cout << endl;
+	delete testAsteroid;
+	testAsteroid = NULL;
+
+	cout << endl;
+	Asteroid asteroid(3, 20, -5);
+	cout << endl;
+	asteroid.setSize(5);
+	asteroid.setSpeed(-4, 20);
+	cout << endl;
+	asteroid.~Asteroid();
+	
+	cin.get();
+	return 0;
+}
